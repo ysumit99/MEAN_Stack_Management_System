0ad5ad0e4eb3c2f6c587bc39ea6205addd3af4ad
ysumit99 <ysumit99@gmail.com>
2019-03-02 00:16:09 +0530

    Initial Node config

---
+++ models/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URI,
+  err => {
+    if (!err) {
+      console.log("MOngoDb connection succeeded!");
+    } else {
+      console.log(
+        "Error in MongoDB connection : " + JSON.stringify(err, undefined, 2)
+      );
+    }
+  },
+  { useNewUrlParser: true }
+);
